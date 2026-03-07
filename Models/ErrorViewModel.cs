c444b0acd01af0667f9bdd9c7913593f9aefe71a
Ramyanaik1 <Ramyanaik1@users.noreply.github.com>
2026-03-07 12:49:16 +0530

    Initial commit - ECartBulkyWebBooks project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ECartBulkyWebBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
