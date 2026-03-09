5a615b466195da4c138f2d67d52bba0d5123eb95
Ramyanaik1 <Ramyanaik1@users.noreply.github.com>
2026-03-09 10:06:26 +0530

    Add Categories controller, views, and MySQL setup

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECartBulkyWebBooks.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
